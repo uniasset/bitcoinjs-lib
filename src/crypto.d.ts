deefe4b1d44aea1dfd08137dd302c393c01649c4
S <ramires9@pm.me>
2023-01-23 18:35:19 +0300

    Regenerated sources

--- src/crypto.d.ts
+++ src/crypto.d.ts
@@ -5,8 +5,8 @@ export declare function sha256(buffer: Buffer): Buffer;
 export declare function hash160(buffer: Buffer): Buffer;
 export declare function hash256(buffer: Buffer): Buffer;
 export declare const TAGS: readonly ["BIP0340/challenge", "BIP0340/aux", "BIP0340/nonce", "TapLeaf", "TapBranch", "TapSighash", "TapTweak", "KeyAgg list", "KeyAgg coefficient"];
-export type TaggedHashPrefix = typeof TAGS[number];
-type TaggedHashPrefixes = {
+export declare type TaggedHashPrefix = typeof TAGS[number];
+declare type TaggedHashPrefixes = {
     [key in TaggedHashPrefix]: Buffer;
 };
 /** An object mapping tags to their tagged hash prefix of [SHA256(tag) | SHA256(tag)] */
